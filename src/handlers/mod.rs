c35db8611fabcc3faa2378c968cf6a2fedbba458
Jean Souza <jeancfsouza@gmail.com>
2024-04-15 23:09:57 -0300

    feat: add persistence

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -17,17 +17,14 @@ pub async fn delete_question(Json(question_uuid): Json<QuestionId>) {
 
 // ---- CRUD for Answers ----
 
-// TODO: Create a POST route to /answer which accepts an `Answer` and returns `AnswerDetail` as JSON.
-//       The handler function should be called `create_answer`.
-//
-//       hint: this function should look very similar to the create_question function above
+pub async fn create_answer(Json(answer): Json<Answer>) -> impl IntoResponse {
+  todo!()
+}
 
-// TODO: Create a GET route to /answers which accepts an `QuestionId` and returns a vector of `AnswerDetail` as JSON.
-//       The handler function should be called `read_answers`.
-//
-//       hint: this function should look very similar to the read_questions function above
+pub async fn read_answers(Json(question_id): Json<QuestionId>) -> impl IntoResponse {
+  todo!()
+}
 
-// TODO: Create a DELETE route to /answer which accepts an `AnswerId` and does not return anything.
-//       The handler function should be called `delete_answer`.
-//
-//       hint: this function should look very similar to the delete_question function above
+pub async fn delete_answer(Json(answer_id): Json<AnswerId>) {
+  todo!()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,49 @@
-use std::net::SocketAddr;
+use std::env;
+use sqlx::postgres::{PgPoolOptions};
 
 use axum::{
-    routing::{delete, get, post},
-    Router,
+  routing::{delete, get, post},
+  Router,
 };
 
 mod handlers;
 mod models;
 
+extern crate pretty_env_logger;
+#[macro_use] extern crate log;
+
 use handlers::*;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/question", post(create_question))
-        .route("/questions", get(read_questions))
-        .route("/question", delete(delete_question))
-        .route("/answer", post(create_answer))
-        .route("/answers", get(read_answers))
-        .route("/answer", delete(delete_answer));
+  pretty_env_logger::init();
+  dotenvy::dotenv().unwrap();
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
-        .await
-        .unwrap();
+  let url = env::var("DATABASE_URL").unwrap();
 
-    axum::serve(listener, app).await.unwrap();
+  let pool = PgPoolOptions::new()
+    .max_connections(5)
+    .connect(&url).await.unwrap();
+
+  let recs = sqlx::query!("SELECT * FROM questions")
+    .fetch_all(&pool)
+    .await
+    .unwrap();
+
+  info!("********* Question Records *********");
+  info!("{:?}", recs);
+
+  let app = Router::new()
+      .route("/question", post(create_question))
+      .route("/questions", get(read_questions))
+      .route("/question", delete(delete_question))
+      .route("/answer", post(create_answer))
+      .route("/answers", get(read_answers))
+      .route("/answer", delete(delete_answer));
+
+  let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+      .await
+      .unwrap();
+
+  axum::serve(listener, app).await.unwrap();
 }
