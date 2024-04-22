2c03efa1c39f088e9c5dc73756ba1ff4d72362de
Jean Souza <jeancfsouza@gmail.com>
2024-04-22 20:22:10 -0300

    chore: implement inner handlers

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,49 @@
-use std::env;
-use sqlx::postgres::{PgPoolOptions};
+#[macro_use]
+extern crate log;
+
+extern crate pretty_env_logger;
+
+use std::sync::Arc;
 
 use axum::{
-  routing::{delete, get, post},
-  Router,
+    routing::{delete, get, post},
+    Router,
 };
+use dotenvy::dotenv;
+
+use persistance::{
+    answers_dao::{AnswersDao, AnswersDaoImpl},
+    questions_dao::{QuestionsDao, QuestionsDaoImpl},
+};
+use sqlx::postgres::PgPoolOptions;
 
 mod handlers;
 mod models;
 mod persistance;
 
-extern crate pretty_env_logger;
-#[macro_use] extern crate log;
-
 use handlers::*;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub questions_dao: Arc<dyn QuestionsDao + Send + Sync>,
+    pub answers_dao: Arc<dyn AnswersDao + Send + Sync>,
+}
 
 #[tokio::main]
 async fn main() {
   pretty_env_logger::init();
-  dotenvy::dotenv().unwrap();
+  dotenv().ok();
 
-  let url = env::var("DATABASE_URL").unwrap();
+  let pool = PgPoolOptions::new()
+      .max_connections(5)
+      .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set."))
+      .await
+      .expect("Failed to create Postgres connection pool!");
 
-  
+  let questions_dao = todo!(); // create a new instance of QuestionsDaoImpl passing in `pool` (use the clone method)
+  let answers_dao = todo!(); // create a new instance of AnswersDaoImpl passing in `pool`
+
+  let app_state = todo!(); // create a new instance of AppState
 
   let app = Router::new()
       .route("/question", post(create_question))
@@ -30,7 +51,9 @@ async fn main() {
       .route("/question", delete(delete_question))
       .route("/answer", post(create_answer))
       .route("/answers", get(read_answers))
-      .route("/answer", delete(delete_answer));
+      .route("/answer", delete(delete_answer))
+      // The with_state method allows us to add state to the state managed by this instance of Axum. Then we can use this state in the handlers.
+      .with_state(todo!()); // pass in `app_state` as application state.
 
   let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
       .await
